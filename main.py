f0fed1bd8c080b83fc266f1cd4d9601c5f712946
rcss <aponabid154@gmail.com>
2023-10-10 02:05:21 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.color("red")
+turtle.speed(10)
+#
+# #Let's draw a squre
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.done()
+
+# for colours in ["white", "red", "blue", "green", "pink", "yellow"]:
+#     turtle.color(colours)
+#     turtle.circle(100)
+#     turtle.left(60)
+# turtle.done()
+for i in range(6):
+    for colours in ["white", "red", "blue", "green", "grey", "yellow"]:
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
